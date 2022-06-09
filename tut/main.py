db055d336085c9d671090e29f524109395cd3861
shalant <doug.rosenberg@gmail.com>
2022-06-09 18:45:16 -0500

    60

--- tut/main.py
+++ tut/main.py
@@ -1,4 +1,8 @@
+import csv
+
 class Item:
+    pay_rate = 0.8 # The pay rate after 20% discount
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
         # Run validations to the received arguments
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
@@ -9,12 +13,31 @@ class Item:
         self.price = price
         self.quantity = quantity
 
+        # Actions to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
+    
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+        
+        for item in items:
+            print(item)
 
-item1 = Item('Phone', 100, 1)
-item2 = Item('Laptop', 1000, 3)
+    def __repr__(self):
+        return f'Item("{self.name}", {self.price}, {self.quantity})'
 
-print(item1.calculate_total_price())
-print(item2.calculate_total_price())
+# item1 = Item('Phone', 100, 1)
+# item2 = Item('Laptop', 1000, 3)
+# item3 = Item('Cable', 10, 5)
+# item4 = Item('Mouse', 50, 5)
+# item5 = Item('Keyboard', 75, 5)
+
+Item.instantiate_from_csv()

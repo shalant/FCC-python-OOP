a273f98efd2329e18bdb507330fc0cde35edbb64
shalant <doug.rosenberg@gmail.com>
2022-06-10 08:55:43 -0500

    151

--- tut/main.py
+++ tut/main.py
@@ -1,55 +1,9 @@
-import csv
-from operator import truediv
+from item import Item
 
-class Item:
-    pay_rate = 0.8 # The pay rate after 20% discount
-    all = []
-    def __init__(self, name: str, price: float, quantity=0):
-        # Run validations to the received arguments
-        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to zero!"
+item1 = Item('MyItem', 750)
 
-        # Assign to self object
-        self.name = name
-        self.price = price
-        self.quantity = quantity
+# Setting an Attribute
+item1.name = 'OtherItem'
 
-        # Actions to execute
-        Item.all.append(self)
-
-    def calculate_total_price(self):
-        return self.price * self.quantity
-    
-    def apply_discount(self):
-        self.price = self.price * self.pay_rate
-
-    @classmethod
-    def instantiate_from_csv(cls):
-        with open('items.csv', 'r') as f:
-            reader = csv.DictReader(f)
-            items = list(reader)
-        
-        for item in items:
-            Item(
-                name=item.get('name'),
-                price=float(item.get('price')),
-                quantity=int(item.get('quantity'))
-            )
-
-    @staticmethod
-    def is_integer(num):
-        # We will count out the floats that are point zero
-        # i.e.: 5.0, 10.0
-        if isinstance(num, float):
-            # Count out the floats that are point zero
-            return num.is_integer()
-        elif isinstance(num, int):
-            return True
-        else:
-            return False
-
-    def __repr__(self):
-        return f'Item("{self.name}", {self.price}, {self.quantity})'
-
-print(Item.is_integer(7.0))
-
+# Getting an Attribute
+print(item1.name)

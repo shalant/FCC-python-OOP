786e4d4aa0a51f5f06f553549fdc32cdb46bb154
shalant <doug.rosenberg@gmail.com>
2022-06-10 10:58:06 -0500

    finished great tutorial

--- tut/main.py
+++ tut/main.py
@@ -1,9 +1,8 @@
-from item import Item
+from phone import Phone
+from keyboard import Keyboard
 
-item1 = Item('MyItem', 750)
+item1 = Keyboard('jscKeyboard', 1000, 3)
 
-# Setting an Attribute
-item1.name = 'OtherItem'
+item1.apply_discount()
 
-# Getting an Attribute
-print(item1.name)
+print(item1.price)
